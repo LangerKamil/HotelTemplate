11577859705efcdfb11ed91dbb40f03db5a7bac6
envelope201 <langer.kamill@gmail.com>
2018-12-10 08:01:18 +0100

    Adding DTOs to the project

---
+++ HotelApplication/DTOs/CustomerDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelApplication.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string IDNumber { get; set; }
+
+
+    }
+}
