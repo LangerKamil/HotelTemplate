8e064858bfa3ca745a24d500a3628eb3d013a94e
envelope201 <langer.kamill@gmail.com>
2018-12-10 08:08:09 +0100

    Installing and configuring AutoMapper, Mapping Customers with CustomersDTO Model using AutoMapper.

---
+++ HotelApplication/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using HotelApplication.Models;
+using HotelApplication.DTOs;
+
+namespace HotelApplication.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDTO>();
+            Mapper.CreateMap<CustomerDTO, Customer>();
+        }
+    }
+}
